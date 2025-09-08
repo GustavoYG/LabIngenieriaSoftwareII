c34c82d2ad7e5b2fe2dcfecf97e0cbdcd728c788
GustavoYG <ryavar@unsa.edu.pe>
2025-09-08 14:38:06 -0500

    cambio variables

--- ejercicio.cpp
+++ ejercicio.cpp
@@ -2,21 +2,21 @@
 using namespace std;
 
 int main() {
-    int n;
+    int num;
     cout << "¿Cuantos numeros vas a ingresar?: ";
-    cin >> n;
+    cin >> num;
 
     double suma = 0, numero;
 
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= num; i++) {
         cout << "Numero " << i << ": ";
         cin >> numero;
         suma += numero;
     }
 
-    double media = suma / n;
+    double media = suma / num;
 
-    cout << "La media de los " << n << " numeros es: " << media << endl;
+    cout << "La media de los " << num << " numeros es: " << media << endl;
 
     return 0;
 }
